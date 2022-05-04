380a5ec505eb21fd68b1ee869b102912e449a395
Bruno Bastos Duarte <bastosduartebruno@gmail.com>
2022-05-04 13:19:26 -0300

    envio de e-mail configurado

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
 import { prisma } from './prisma';
+import nodemailer from 'nodemailer';
 import express from 'express';
 
 const app = express();
 
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "8273ff80deb8eb",
+    pass: "a32748a4fb6943"
+  }
+});
 
 // GET = Buscar informação
 // POST = Cadastrar informação
@@ -20,6 +30,18 @@ app.post('/feedbacks', async (req, res) => {
       comment,
       screenshot,
     }
+  })
+
+  await transport.sendMail({
+    from: 'Equipe Feedget <equipefeedget@gmail.com>',
+    to: 'Bruno Duarte <bastosduartebruno@gmail.com>',
+    subject: 'Novo feedback',
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário ${comment}</p>`,
+      `<div>`,
+    ].join('\n')
   })
 
   return res.status(201).json({ data:feedback });
