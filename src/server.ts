df1d5cf654c73f0f2674873ec2e1922678229e12
Bruno Bastos Duarte <bastosduartebruno@gmail.com>
2022-05-04 12:58:51 -0300

    rota de feedback configurado

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,28 @@
+import { prisma } from './prisma';
 import express from 'express';
 
 const app = express();
 
-app.get('/users', (req, res) => {
-  return res.send('Hello World!');
+app.use(express.json());
+
+// GET = Buscar informação
+// POST = Cadastrar informação
+// PUT = Atualizar informação de uma entidade
+// PATCH = Atualizar informação de uma única entidade
+// DELETE = Deletar informação de uma entidade
+
+app.post('/feedbacks', async (req, res) => {
+  const { type, comment, screenshot } = req.body;
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    }
+  })
+
+  return res.status(201).json({ data:feedback });
 })
 
 app.listen(3333, () => {
